ab4e0479bc318b3c4bb228a37ce155462bd9f684
Roman Poslavski <swapper9@gmail.com>
2020-01-25 23:32:18 +0300

    speeding scan with fixedThreadPool

--- src/main/java/swapper9/moviedatabase/DataConfig.java
+++ src/main/java/swapper9/moviedatabase/DataConfig.java
@@ -9,35 +9,51 @@ import org.springframework.core.env.Environment;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.sql.DataSource;
+import java.util.concurrent.Executor;
 
 @Configuration
+@EnableAsync
 @ComponentScan("swapper9.moviedatabase.domain")
 @EnableJpaRepositories(basePackages = "swapper9.moviedatabase.repository")
 @PropertySource("classpath:application.properties")
 @EnableTransactionManagement
 public class DataConfig {
 
-  @Autowired
-  private Environment env;
+    @Autowired
+    private Environment env;
 
-  @Bean
-  public DataSource dataSource() {
-    DriverManagerDataSource dataSource = new DriverManagerDataSource ();
-    dataSource.setDriverClassName(env.getProperty("spring.datasource.driver-class-name"));
-    dataSource.setUrl(env.getProperty("spring.datasource.url"));
-    dataSource.setUsername(env.getProperty("spring.datasource.username"));
-    dataSource.setPassword(env.getProperty("spring.datasource.password"));
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource ();
+        dataSource.setDriverClassName(env.getProperty("spring.datasource.driver-class-name"));
+        dataSource.setUrl(env.getProperty("spring.datasource.url"));
+        dataSource.setUsername(env.getProperty("spring.datasource.username"));
+        dataSource.setPassword(env.getProperty("spring.datasource.password"));
 
-    return dataSource;
-  }
+        return dataSource;
+    }
 
-  @Bean
-  public JdbcTemplate jdbcTemplate(DataSource dataSource) {
-    return new JdbcTemplate(dataSource);
-  }
+    @Bean
+    public JdbcTemplate jdbcTemplate(DataSource dataSource) {
+        return new JdbcTemplate(dataSource);
+    }
+
+    @Bean
+    public Executor asyncExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(4);
+        executor.setMaxPoolSize(4);
+        executor.setQueueCapacity(50);
+        executor.setThreadNamePrefix("AsynchThread-");
+        executor.initialize();
+        return executor;
+    }
+
 
   /*@Bean
   @Autowired
--- src/main/java/swapper9/moviedatabase/ScanCinemate.java
+++ src/main/java/swapper9/moviedatabase/ScanCinemate.java
@@ -21,113 +21,118 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.Optional;
 import java.util.Scanner;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicInteger;
 
 @Service
 @Log4j2
 public class ScanCinemate {
 
-  @Autowired
-  private MovieRepository movieRepository;
+    @Autowired
+    private MovieRepository movieRepository;
 
+    volatile AtomicInteger page = new AtomicInteger(0);
+    volatile AtomicInteger numScan = new AtomicInteger(0);
 
-  /**
-   * Первый параметр - PASSKEY юзера, который можно взять на http://cinemate.cc/preferences/#api
-   * Скачивается база, доступная на http://cinemate.cc/profile/{nickname}/view_history/
-   * @param passkey
-   * @throws IOException
-   */
-  public void addMoviesToDb(String passkey) throws IOException {
-
-    int page = 0;
-    int numScan = 1;
-    while (true) {
-      JsonObject jsonObject = JsonParser
-        .parseString(getHtmlPageFromUrl(new URL("http://api.cinemate.cc/account.votes?passkey=" + passkey + "&format=json&page=" + page)))
-        .getAsJsonObject();
-      JsonArray jsonVoteArray = (JsonArray)jsonObject.get("vote");
-      if (jsonVoteArray.size() < 1) break;
-
-      for (int i = 0; i < jsonVoteArray.size(); i++) {
-        JsonObject jsonMovieObject = (JsonObject)jsonVoteArray.get(i);
-        String movie_url = jsonMovieObject.get("movie").getAsJsonObject().get("url").getAsString();
-        String title_russian = jsonMovieObject.get("movie").getAsJsonObject().get("title_russian").getAsString();
-        String title_original = Optional.ofNullable(movie_url)
-          .map(u -> getMetaTag(getDocument(u)))
-          .map(d -> d.split(" / "))
-          .map(a -> a[a.length - 1].substring(0, a[a.length - 1].length() - 6).trim())
-          .orElse(null);
-        String year = jsonMovieObject.get("movie").getAsJsonObject().get("year").getAsString();
-        String poster_url_small = "https:".concat(jsonMovieObject.get("movie").getAsJsonObject().get("poster").getAsJsonObject().get("small").getAsJsonObject().get("url").getAsString());
-        String poster_url_big = "https:".concat(jsonMovieObject.get("movie").getAsJsonObject().get("poster").getAsJsonObject().get("big").getAsJsonObject().get("url").getAsString());
-        Movie movieDb = movieRepository.findByUrl(movie_url);
-        if (movieDb == null) {
-          Movie movie = new Movie(movie_url, title_russian, title_original, year, poster_url_small, poster_url_big);
-          movieRepository.save(movie);
-          System.out.println("#" + numScan++ + " saving movie#" + movie.getId());
-        } else {
-          System.out.println("#" + numScan++ + " skipped, duplicated url: " + movie_url);
-          break;
+    /**
+     * Первый параметр - PASSKEY юзера, который можно взять на http://cinemate.cc/preferences/#api
+     * Скачивается база, доступная на http://cinemate.cc/profile/{nickname}/view_history/
+     * @param passkey
+     * @throws IOException
+     */
+    public void addMoviesToDb(String passkey) throws IOException {
+        ExecutorService executor = Executors.newFixedThreadPool(50);
+        while (true) {
+            JsonObject jsonObject = JsonParser
+                    .parseString(getHtmlPageFromUrl(new URL("http://api.cinemate.cc/account.votes?passkey=" + passkey + "&format=json&page=" + page)))
+                    .getAsJsonObject();
+            JsonArray jsonVoteArray = (JsonArray)jsonObject.get("vote");
+            if (jsonVoteArray.size() < 1) break;
+            executor.execute(() -> scanPage(jsonVoteArray, numScan));
+            page.getAndIncrement();
         }
-      }
-      page++;
+        System.out.println("End of scan");
     }
-  }
 
-  private static byte[] savePoster(String image_url) throws IOException {
-    URL url = new URL(image_url);
-    HttpURLConnection httpcon = (HttpURLConnection) url.openConnection();
-    httpcon.addRequestProperty("User-Agent", "Mozilla/4.76");
-    InputStream in = new BufferedInputStream(httpcon.getInputStream());
-    ByteArrayOutputStream out = new ByteArrayOutputStream();
-    byte[] buf = new byte[2048];
-    int n = 0;
-    while (-1!=(n=in.read(buf))) {
-      out.write(buf, 0, n);
+    public void scanPage(JsonArray jsonVoteArray, AtomicInteger numScan) {
+        for (int i = 0; i < jsonVoteArray.size(); i++) {
+            JsonObject jsonMovieObject = (JsonObject)jsonVoteArray.get(i);
+            String movie_url = jsonMovieObject.get("movie").getAsJsonObject().get("url").getAsString();
+            String title_russian = jsonMovieObject.get("movie").getAsJsonObject().get("title_russian").getAsString();
+            String title_original = Optional.ofNullable(movie_url)
+                    .map(u -> getMetaTag(getDocument(u)))
+                    .map(d -> d.split(" / "))
+                    .map(a -> a[a.length - 1].substring(0, a[a.length - 1].length() - 6).trim())
+                    .orElse(null);
+            String year = jsonMovieObject.get("movie").getAsJsonObject().get("year").getAsString();
+            String poster_url_small = "https:".concat(jsonMovieObject.get("movie").getAsJsonObject().get("poster").getAsJsonObject().get("small").getAsJsonObject().get("url").getAsString());
+            String poster_url_big = "https:".concat(jsonMovieObject.get("movie").getAsJsonObject().get("poster").getAsJsonObject().get("big").getAsJsonObject().get("url").getAsString());
+            Movie movieDb = movieRepository.findByUrl(movie_url);
+            if (movieDb == null) {
+                Movie movie = new Movie(movie_url, title_russian, title_original, year, poster_url_small, poster_url_big);
+                movieRepository.save(movie);
+                numScan.getAndIncrement();
+                System.out.println("Saving movie #" + movie.getId());
+            }
+        }
     }
-    out.close();
-    in.close();
-    return out.toByteArray();
-  }
+
+    private static byte[] savePoster(String image_url) throws IOException {
+        URL url = new URL(image_url);
+        HttpURLConnection httpcon = (HttpURLConnection) url.openConnection();
+        httpcon.addRequestProperty("User-Agent", "Mozilla/4.76");
+        InputStream in = new BufferedInputStream(httpcon.getInputStream());
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        byte[] buf = new byte[2048];
+        int n = 0;
+        while (-1!=(n=in.read(buf))) {
+            out.write(buf, 0, n);
+        }
+        out.close();
+        in.close();
+        return out.toByteArray();
+    }
 
 
-  private static String getMetaTag(Document doc) {
-    Elements elements = doc.select("meta[name=description]");
-    for (Element element : elements) {
-      final String s = element.attr("content");
-      if (s != null) return s;
+    private static String getMetaTag(Document doc) {
+        Elements elements = doc.select("meta[name=description]");
+        for (Element element : elements) {
+            final String s = element.attr("content");
+            if (s != null) return s;
+        }
+        elements = doc.select("meta[property=description]");
+        for (Element element : elements) {
+            final String s = element.attr("content");
+            if (s != null) return s;
+        }
+        return null;
     }
-    elements = doc.select("meta[property=description]");
-    for (Element element : elements) {
-      final String s = element.attr("content");
-      if (s != null) return s;
-    }
-    return null;
-  }
 
-  private static Document getDocument(String url) {
-    Document doc = null;
-    try {
-      doc = Jsoup.connect(url).get();
-    } catch (IOException e) {
-      e.printStackTrace();
+    private static Document getDocument(String url) {
+        Document doc = null;
+        try {
+            doc = Jsoup.connect(url).get();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return doc;
     }
-    return doc;
-  }
 
-  private static String getHtmlPageFromUrl(URL url) throws IOException {
-    HttpURLConnection conn = (HttpURLConnection)url.openConnection();
-    conn.setRequestMethod("GET");
-    conn.connect();
-    int respCode = conn.getResponseCode();
-    if (respCode != 200) {
-      throw new RuntimeException("HttpResponseCode: " + respCode);
+    private static String getHtmlPageFromUrl(URL url) throws IOException {
+        HttpURLConnection conn = (HttpURLConnection)url.openConnection();
+        conn.setRequestMethod("GET");
+        conn.connect();
+        int respCode = conn.getResponseCode();
+        if (respCode != 200) {
+            throw new RuntimeException("HttpResponseCode: " + respCode);
+        }
+        String inline = "";
+        Scanner scanner = new Scanner(url.openStream());
+        while (scanner.hasNext()) {
+            inline += scanner.nextLine();
+        }
+        scanner.close();
+        return inline;
     }
-    String inline = "";
-    Scanner scanner = new Scanner(url.openStream());
-    while (scanner.hasNext()) {
-      inline += scanner.nextLine();
-    }
-    scanner.close();
-    return inline;
-  }
 }
